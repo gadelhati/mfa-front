176e7ee3563458d625028455d8beb63a8c1f31e1
GadelhaTI <gadelha.ti@gmail.com>
2025-01-07 01:15:19 -0300

    feature: input

--- src/container/page/login.tsx
+++ src/container/page/login.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, KeyboardEvent, useState } from 'react'
 import { initialUser, initialUserAuth, User, UserAuth } from '../../component/user'
 import { login } from '../../service/service.crud'
-import '../template/input.css'
+import { GInput } from '../template/input'
 import './login.css'
 
 export const Login = () => {
@@ -34,19 +34,17 @@ export const Login = () => {
             <button onClick={()=>setMode(false)} disabled={!mode}>Signup</button>
             <div className={mode?`card content`:`card content change`}>
                 <div className='card front container'>
-                    <input onChange={handleInputChangeAuth} type='text' name='username' id='username' placeholder='username' />
-                    <input onChange={handleInputChangeAuth} type='password' name='password' id='password' placeholder='*' />
-                    <input onChange={handleInputChangeAuth} type='text' name='totpKey' id='totpKey' placeholder='6 dígitos' />
+                    <GInput onChange={handleInputChangeAuth} name='username'></GInput>
+                    <GInput onChange={handleInputChangeAuth} type="password" name='password'></GInput>
+                    <GInput onChange={handleInputChangeAuth} name='totpKey'></GInput>
+                    <button onClick={loginUser}>button</button>
                 </div>
                 <div className='card back container'>
-                    <input onChange={handleInputChange} type='text' name='username' id='username' placeholder='username' />
-                    <input onChange={handleInputChange} type='password' name='password' id='password' placeholder='*' />
-                    <input onChange={handleInputChange} type='text' name='email' id='email' placeholder='email' />
+                    <GInput onChange={handleInputChange} name='username'></GInput>
+                    <GInput onChange={handleInputChange} type="password" name='password'></GInput>
+                    <GInput onChange={handleInputChange} name='email'></GInput>
                 </div>
             </div>
-            <button className="primary" onClick={loginUser}>button</button>
-            <div>user auth: {JSON.stringify(userAuth)}</div>
-            <div>user: {JSON.stringify(user)}</div>
         </div>
     )
 }
---
+++ src/container/template/input.tsx
@@ -0,0 +1,11 @@
+import { InputHTMLAttributes } from "react"
+import '../template/input.css'
+
+export const GInput: React.FC<InputHTMLAttributes<HTMLInputElement>> = ({name, ...props}) => {
+    return (
+        <div className="container">
+            <input required {...props} name={name} id={name} ></input>
+            {name && <label htmlFor={name}>{name.charAt(0)+name.slice(1)}</label>}
+        </div>
+    )
+}
