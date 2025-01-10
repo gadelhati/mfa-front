5de2771d9bd889bb85054c6e36f006fa895f7e94
GadelhaTI <gadelha.ti@gmail.com>
2025-01-10 01:24:37 -0300

    style: improve login style.

--- src/container/page/login.tsx
+++ src/container/page/login.tsx
@@ -31,25 +31,29 @@ export const Login = () => {
     }
     return (
         <div className='wrapper' onKeyDown={submit}>
-            <button onClick={()=>setMode(true)} disabled={mode}>Sign</button>
-            <button onClick={()=>setMode(false)} disabled={!mode}>Signup</button>
             <div className={mode?`card content`:`card content change`}>
                 <div className='card front'>
+                    {/* <button onClick={()=>setMode(false)} disabled={!mode}>Signup</button> */}
+                    <h3 className='sign'>Signin</h3>
                     <GInput onChange={handleInputChangeAuth} name='username'></GInput>
                     <GInput onChange={handleInputChangeAuth} type="password" name='password'></GInput>
                     <GInput onChange={handleInputChangeAuth} name='totpKey'></GInput>
-                    <div>
-                        <input type="checkbox" id="remember" name="remember" checked={remember} onClick={()=>setRemember(!remember)} />
-                        <label htmlFor="remember">Remember me</label>
-                        <a href='#'>forgot password</a>
-                    </div>
+                    <footer>
+                        <div>
+                            <input type="checkbox" id="remember" name="remember" checked={remember} onClick={()=>setRemember(!remember)} />
+                            <label htmlFor="remember">Remember me</label>
+                        </div>
+                        <a href='#'><label>forgot password</label></a>
+                    </footer>
                     <button onClick={loginUser}>button</button>
                 </div>
-                <div className='card back'>
+                {/* <div className='card back'>
+                    <h2 className='sign'>Signup</h2>
+                    <button onClick={()=>setMode(true)} disabled={mode}>Sign</button>
                     <GInput onChange={handleInputChange} name='username'></GInput>
                     <GInput onChange={handleInputChange} type="password" name='password'></GInput>
                     <GInput onChange={handleInputChange} name='email'></GInput>
-                </div>
+                </div> */}
             </div>
         </div>
     )
