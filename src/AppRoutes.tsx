db15e1ce9c9529ac20f8492f70872fa314f19629
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2025-01-16 10:57:06 -0300

    refactor: change file name

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,16 +1,20 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { AuthProvider } from "./assets/hook/useProvider";
+import { AuthContext, AuthProvider } from "./assets/hook/useProvider";
 import { Login } from "./container/page/login";
 import { Profile } from "./container/page/profile";
+import { useContext } from "react";
+import { Page } from "./container/page/page";
 
 export const AppRoutes = () => {
+    const { accessToken } = useContext(AuthContext)
     return (
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
                     <Route path="*" element={<Login />}></Route>
                     <Route path="/" element={<Login />}></Route>
-                    <Route path="/profile" element={<Profile />}></Route>
+                    <Route path="/profile" element={<Profile ui={accessToken} />}></Route>
+                    <Route path="/page" element={<Page ui={accessToken} />}></Route>
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
