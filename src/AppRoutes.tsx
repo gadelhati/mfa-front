4ddc94d52e260d20cf3f8387cf78d3e0c3960876
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2025-01-17 09:51:17 -0300

    feature: limited access by expiration token

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,20 +1,19 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { AuthContext, AuthProvider } from "./assets/hook/useProvider";
+import { AuthProvider } from "./assets/hook/useProvider";
 import { Login } from "./container/page/login";
 import { Profile } from "./container/page/profile";
-import { useContext } from "react";
-import { Page } from "./container/page/page";
+import { isValidToken } from "./service/service.token";
 
 export const AppRoutes = () => {
-    const { accessToken } = useContext(AuthContext)
     return (
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
                     <Route path="*" element={<Login />}></Route>
                     <Route path="/" element={<Login />}></Route>
-                    <Route path="/profile" element={<Profile ui={accessToken} />}></Route>
-                    <Route path="/page" element={<Page ui={accessToken} />}></Route>
+                    {isValidToken() &&
+                        <Route path="/profile" element={<Profile />}></Route>
+                    }
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
