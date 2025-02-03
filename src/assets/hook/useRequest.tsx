8fd0a6cfabbdfc4a1c4c6e24c117edc1160bc8c7
GadelhaTI <gadelha.ti@gmail.com>
2025-02-03 02:07:09 -0300

    chore: add abortController

--- src/assets/hook/useRequest.tsx
+++ src/assets/hook/useRequest.tsx
@@ -10,14 +10,14 @@ export const useRequest = <T extends Object>(url: string, value?: string, page?:
     // const [pageable, setPageable] = useState<Pageable>(initialPageable)
     const [pageable, setPageable] = useState<Page>(initialPage)
     const [error, setError] = useState<ErrorMessage[]>([initialErrorMessage])
-    const controller = new AbortController();
 
     useEffect(() => {
-        retrieve(url)
+        const controller = new AbortController();
+        retrieve(url, controller.signal);
         return (() => {
             controller.abort()
         })
-    }, [])
+    }, [url, value, page, size, sort])
     const retrieve = useCallback( async (uri: string, signal?: AbortSignal) => {
         await api.get<{ content: T[]; page: Page }>(`/${uri}${value ? `?value=${value}` : ""}`,
             {
