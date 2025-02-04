725f7da18d7539b4c5d44ca7067af3f6d5ba7963
GadelhaTI <gadelha.ti@gmail.com>
2025-02-04 00:14:52 -0300

    refactor: improving code

--- src/container/template/GenericComponent.tsx
+++ src/container/template/GenericComponent.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRequest } from "../../assets/hook/useRequest"
 import { intialSearch, Search } from "../../component/search"
 import { DataTable } from "./DataTable";
 import { useInput } from "../../assets/hook/useInput";
 import '../template/inputgroup.css'
+import { ErrorMessage } from "../../assets/error/errorMessage";
+import { initialErrorMessage } from "../../assets/error/errorMessage.initial";
 
 interface Data<T extends Object> {
     object: T,
@@ -11,13 +13,18 @@ interface Data<T extends Object> {
 }
 
 export const GenericComponent = <T extends Object>(object: Data<T>) => {
-    const controller = new AbortController();
     // const [isInterface] = useIsInterface<T, User>(initialRole, initialUser)
     const { state: search, handleInput: handleSearch } = useInput<Search>(intialSearch)
     const { states, pageable, retrieve } = useRequest<T>(object.url, search.value, search.page, search.size, { key: search.key, order: search.order })
+    const [error, setError] = useState<ErrorMessage[]>([initialErrorMessage])
 
     useEffect(() => {
-        retrieve(object.url)
+        const controller = new AbortController();
+        retrieve(object.url, controller.signal).catch((error)=>{
+            if (!controller.signal.aborted) {
+                setError([{message: error}])
+            }
+        })
         return (() => {
             controller.abort()
         })
@@ -30,12 +37,14 @@ export const GenericComponent = <T extends Object>(object: Data<T>) => {
                 }))}
             </select> */}
             {/* FIND ON DATA TABLE */}
-            {intialSearch !== undefined && Object.entries(search).map(([key, value]: [string, any]) => {
-                return <span key={key + 'span'} className={'inputgroup tooltip'} data-tip={[]} style={{ display: 'flex' }}>
-                    <input key={key} type={typeof value} name={key} value={value} onChange={handleSearch} placeholder={key} ></input>
+            {Object.keys(intialSearch).map((key) => {
+                const value = search[key as keyof Search]
+                return <span key={key} className={'inputgroup tooltip'} data-tip={[]} style={{ display: 'flex' }}>
+                    <input type={typeof value} name={key} value={value} onChange={handleSearch} placeholder={key} ></input>
                     <label htmlFor={key}>{key}</label>
                 </span>
             })}
+            {/* {error && <div className="error-message">{JSON.stringify(error[0].message)}</div>} */}
             <DataTable object={object.object} list={states} pageable={pageable} search={search} url={object.url} />
         </>
     )
