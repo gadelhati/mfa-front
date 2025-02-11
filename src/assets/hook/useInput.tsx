cb116e01b9341382f380d269bff0f2cefcacd96f
GadelhaTI <gadelha.ti@gmail.com>
2025-02-10 21:58:49 -0300

    fix: only type pattern validation on input

--- src/assets/hook/useInput.tsx
+++ src/assets/hook/useInput.tsx
@@ -13,9 +13,9 @@ export const useInput = <T extends Object>(data: T) => {
     }
     const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
         const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
-        // if (new RegExp(event.target.pattern).test(event.target.value) || event.target.value === '') { 
+        if (new RegExp(event.target.pattern).test(event.target.value) || event.target.value === ``) { 
             setState({ ...state, [event.target.name]: value })
-        // }
+        }
     }
     const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
         api.get<T>(`/${event.target.name}?value=${event.target.value}`, { params: { page: 0, size: 20, sort: `id,ASC` } }
